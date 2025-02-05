f461f64e950dedfd74d857e71e46aa265a4d31ea
Adam Skuta <adam.skuta@gmail.com>
2025-02-05 20:26:54 +0100

    Added lsps

--- init.lua
+++ init.lua
@@ -550,7 +550,9 @@ require("lazy").setup({
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				-- ts_ls = {},
-				--
+				ocamllsp = {},
+				gopls = {},
+				pyright = {},
 
 				lua_ls = {
 					-- cmd = { ... },
