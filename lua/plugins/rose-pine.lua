d3686eca4373f359c9e59c60260b418f693a8a78
Adam Skuta <adam.skuta@ait.ac.at>
2025-09-18 16:04:29 +0200

    Removed italics

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,7 +1,10 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd.colorscheme "rose-pine"
-	end
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup {
+      styles = { italic = false, transparency = true },
+    }
+    vim.cmd.colorscheme "rose-pine"
+  end,
 }
